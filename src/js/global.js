d756ef6c27aa92d5afc909573643fe2d69a62fd3
dudkina_m <dudkina.m.94@gmail.com>
2018-08-21 08:15:07 +0300

    popup, home

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,7 @@
 $(function () {
     sliderInit();
     AOS.init();
+    modalDisplay();
 });
 
 function mapInit() {
@@ -19,6 +20,39 @@ function sliderInit() {
         autoplay: true,
         arrows: false,
         dots: true
+    });
+
+    $('.partners-content__slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 }
 
