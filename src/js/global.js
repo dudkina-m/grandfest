6f03f7173ad2c8ac6773bbb49b834e7e2169541e
dudkina_m <dudkina.m.94@gmail.com>
2018-08-21 09:09:16 +0300

    slider

--- src/js/global.js
+++ src/js/global.js
@@ -23,26 +23,26 @@ function sliderInit() {
     });
 
     $('.partners-content__slider').slick({
-        dots: false,
-        infinite: false,
-        speed: 300,
+        infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: "<button type='button' class='slick-prev'></button>",
+        nextArrow: "<button type='button' class='slick-next'></button>",
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
+                    slidesToScroll: 1,
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 1
                 }
             },
             {
